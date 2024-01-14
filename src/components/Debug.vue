24b36878e311565d4d0a1de62c0f72f99357d05d
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-01-14 14:50:05 -0300

    ADD: Debug component

---
+++ src/components/Debug.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+  import type {Quiz} from "../types/types"
+
+  interface Props {
+    data: Quiz
+  }
+  const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="debug">
+    <pre><code>{{ props.data.answer }}</code></pre>
+  </div>
+</template>
+
+<style lang="scss">
+
+  .debug {
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    transform: translate(0, -50%);
+    background: black;
+    color: rgb(20, 224, 20);
+    max-height: 300px;
+    max-width: 240px;
+    width: 100%;
+    overflow: auto;
+    box-shadow: 2px 4px 4px 2px rgba(0, 0, 0, 0.258);
+    z-index: 99;
+  }
+</style>
