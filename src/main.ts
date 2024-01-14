aa28b6c3b6793f61267faf61f012f7c31229d140
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-01-14 14:47:28 -0300

    ADD: CSS custom properties

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import { createPinia } from "pinia"
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "./assets/scss/main.scss"
 
 import App from "./App.vue"
 import router from "./router"
