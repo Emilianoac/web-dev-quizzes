cfec10727df2d9a51e3dd74ab9a49e4d0a5f34d1
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-01-30 10:03:20 -0300

    UPD: Routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,41 @@
-import { createRouter, createWebHashHistory } from "vue-router"
+import { createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router"
 import Home from "../views/Home.vue"
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/quiz/:id",
+    name: "quiz",
+    props: true,
+    component: () => import("@/views/Quiz.vue")
+  },
+
+  //BACKOFFICE
+  {
+    path: "/backoffice/nueva-categoria",
+    name: "nueva categoria",
+    component: () => import("@/views/backOffice/forms/NewCategory.vue")
+  },
+  {
+    path: "/backoffice/categorias",
+    name: "categorias",
+    component: () => import("@/views/backOffice/Categories.vue")
+  },
+  {
+    path: "/backoffice/editar-quizz/:id",
+    name: "editar quiz",
+    component: () => import("@/views/backOffice/forms/EditQuiz.vue")
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home,
-    },
-    {
-      path: "/quiz/:quizname",
-      name: "quiz",
-      props: true,
-      component: () => import("../views/Quiz.vue")
-    }
-  ]
+  routes: routes
 })
+
 
 export default router
