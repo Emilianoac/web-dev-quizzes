843b23488d643f8cdd47a7c5a555f91a12de96cb
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-01-30 10:21:03 -0300

    UPD: Layout

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -3,17 +3,45 @@
 </script>
 
 <template>
-  <nav class="navbar sticky-top">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
       <RouterLink class="navbar-brand fw-bold" to="/">Web Dev Quizzes</RouterLink>
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" title="Repositorio GitHub" target="_blank" href="https://github.com/Emilianoac/web-dev-quizzes"><i class="fab fa-github"></i></a>
-        </li>
-      </ul>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a 
+              class="nav-link active" 
+              title="Repositorio GitHub" 
+              target="_blank" 
+              href="https://github.com/Emilianoac/web-dev-quizzes">
+              <i class="fab fa-github"></i>
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a 
+              class="nav-link dropdown-toggle" 
+              href="#" 
+              role="button" 
+              data-bs-toggle="dropdown" 
+              aria-expanded="false">
+              Admin
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><RouterLink class="dropdown-item" :to="{name: 'nueva categoria'}">Nueva Categoria</RouterLink></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><RouterLink class="dropdown-item" :to="{name: 'categorias'}">Ver Categorias</RouterLink></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item text-danger" href="#">Salir</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
-   <slot/>
+  <slot/>
 </template>
 
 <style lang="scss">
